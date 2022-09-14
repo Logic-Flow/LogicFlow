365223b1c571af5af724d936c1bd14fa99209653
jerryjiang <jerryjiang@didichuxing.com>
2022-09-14 17:03:39 +0800

    style: 样式、动画优化

--- site/playgroundEx/src/components/SideBar/index.vue
+++ site/playgroundEx/src/components/SideBar/index.vue
@@ -4,13 +4,13 @@
       <div v-for="topic in config.topic" :key="topic.key">
         <div class="menu-item">
           <span
-            class="topic hoverFont"
-            @click.stop="(e) => handleTopicClick(e, topic)"
+            :class="`topic hover-font ${handleIconFold(topic.fold)}`"
+            @click.stop="animations.toggleFold(topic)"
           >
             <strong>
               {{ topic.name }}
             </strong>
-            <span class="foldIcon"> ⌵ </span>
+            <span class="fold-icon"></span>
           </span>
           <CollapseTransition>
             <div :class="`examples-wrap ${topic.name}`" v-show="!topic.fold">
@@ -26,16 +26,16 @@
                   :class="item.name"
                 >
                   <div
-                    @click.stop="(e) => handleTopicClick(e, item)"
-                    class="hoverFont subTitle"
+                    @click.stop="animations.toggleFold(item)"
+                    :class="`hover-font sub-title ${handleIconFold(item.fold)}`"
                   >
                     <span>
                       {{ item.name }}
                     </span>
-                    <span class="foldIcon"> ⌵ </span>
+                    <span class="fold-icon"></span>
                   </div>
                   <CollapseTransition>
-                    <div v-show="!item.fold" class="subItems">
+                    <div v-show="!item.fold" class="sub-items">
                       <div v-for="(e, index) in topic.examples" :key="e.key">
                         <a
                           @click.stop="
@@ -43,7 +43,7 @@
                               `sub-${item.name}-${index}-${e.name}`,
                             )
                           "
-                          :class="`sub-${item.name}-${index}-${e.name} hoverBkg sub-item`"
+                          :class="`sub-${item.name}-${index}-${e.name} hover-bkg sub-item`"
                           :href="`#${item.name}`"
                         >
                           <img
@@ -55,7 +55,7 @@
                     </div>
                   </CollapseTransition>
                 </div>
-                <div v-else :class="`${item.name} hoverBkg`">
+                <div v-else :class="`${item.name} hover-bkg`">
                   <a :href="`#${item.name}`">
                     {{ item.name }}
                   </a>
@@ -66,8 +66,8 @@
         </div>
       </div>
     </div>
-    <div class="foldTool" @click="animations.handleWidthHidden">
-      <span :class="`${widthHidden && 'foldToolTransform'}`"> 〈 </span>
+    <div class="fold-tool" @click="animations.handleWidthHidden">
+      <span :class="`${widthHidden && 'fold-tool-transform'}`"> 〈 </span>
     </div>
   </div>
 </template>
@@ -90,13 +90,17 @@ const initConfig = (data) => {
 initConfig(config.value);
 
 const widthHidden = ref(false);
+
 const props = defineProps({
   type: {
     type: String,
-    default: 'withThumbnail', // 包换缩略图/
-    // default: '123', // 包换缩略图
+    default: 'withThumbnail', // 包含缩略图/
   },
 });
+
+const handleIconFold = (flag) => {
+  return flag ? 'fold-icon-open' : 'fold-icon-collapse';
+};
 
 const useMenuItemEvent = () => {
   const el = reactive({
@@ -114,20 +118,21 @@ const useMenuItemEvent = () => {
     if (el.prev.name === '') {
       el.prev.name = el.cur.name = className;
       el.prev.node = el.cur.node = document.querySelector(`.${className}`);
-      el.cur.node.classList.toggle('itemActive');
+      el.cur.node.classList.toggle('item-active');
     } else {
       el.prev.name = el.cur.name;
       el.prev.node = document.querySelector(`.${el.cur.name}`);
-      el.prev.node.classList.toggle('itemActive');
+      el.prev.node.classList.toggle('item-active');
       el.cur.name = className;
       el.cur.node = document.querySelector(`.${className}`);
-      el.cur.node.classList.toggle('itemActive');
+      el.cur.node.classList.toggle('item-active');
     }
   };
 
   const handleExamplesItemClick = (className) => {
     props.type !== 'withThumbnail' && menuItemEvents.clickItem(className);
   };
+
   return {
     clickItem,
     handleExamplesItemClick,
@@ -142,34 +147,16 @@ const useAnimations = () => {
     widthHidden.value = !widthHidden.value;
   };
 
-  const rotate180 = (e) => {
-    e.classList.toggle('rotate3dX-180');
-  };
-
   const jumpTo = () => {};
 
   return {
     toggleFold,
     handleWidthHidden,
-    rotate180,
   };
 };
 
 const animations = useAnimations();
 const menuItemEvents = useMenuItemEvent();
-
-const handleTopicClick = (e, topic) => {
-  let foldIcon;
-  if (e.path[0]?.classList.contains('hoverFont')) {
-    foldIcon = e.target.lastChild;
-  } else {
-    const father = e.path[1];
-    foldIcon = father.lastChild;
-  }
-  console.log(foldIcon);
-  animations.rotate180(foldIcon);
-  animations.toggleFold(topic);
-};
 
 onMounted(() => {
   config.value.topic.forEach((topic) => {
