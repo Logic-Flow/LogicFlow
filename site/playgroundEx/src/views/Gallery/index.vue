2148034bcbbfa59b69b5eee174dbbc55f4e53362
jerryjiang <jerryjiang@didichuxing.com>
2022-09-21 11:23:11 +0800

    feat: gallery & playground

--- site/playgroundEx/src/views/Gallery/index.vue
+++ site/playgroundEx/src/views/Gallery/index.vue
@@ -40,38 +40,5 @@ const config = inject('originConfig');
 const type = inject('type');
 </script>
 <style lang="scss" scoped>
-.gallery {
-  height: 100%;
-  width: 100%;
-  overflow: scroll;
-  scroll-behavior: smooth;
-}
-.case-wrap {
-  display: flex;
-  flex-wrap: wrap;
-  transition: 0.25s;
-  .case {
-    width: 240px;
-    // height: 180px;
-    // background: #ccc;
-    margin: 10px 15px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    transition: 0.3s;
-    img {
-      width: 240px;
-      height: 160px;
-      transition: 0.3s;
-      border: none;
-    }
-    &:hover {
-      // background-color: #000;
-      img {
-        transform: scale(1.05) translateY(-5px);
-        box-shadow: 0px 3px 10px 1px #e2e2e2;
-      }
-    }
-  }
-}
+@import './style.scss';
 </style>
